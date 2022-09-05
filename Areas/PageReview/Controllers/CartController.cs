8b4d141ce6f437e9e7d7c54f90330f117a872986
Nartkein <94590212+Nartkein@users.noreply.github.com>
2022-09-06 01:03:52 +0700

    Add files via upload

---
+++ Areas/PageReview/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PJC.Models;
+using System;
+
+namespace PJC.Areas.PageReview
+{
+    [Area("PageReview")]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            StoreContext context = HttpContext.RequestServices.GetService(typeof(PJC.Models.StoreContext)) as StoreContext;
+            return View(context.GetSanPham());
+        }
+
+        [HttpPost]
+        public void AddToCart(string code)
+        {
+            System.Diagnostics.Debug.WriteLine(code);
+        }
+    }
+}
